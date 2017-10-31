1a00b6386c7cfbbaa7674170abb5577641f6cea3
Mike Butt <mike.butt@os.uk>
2017-10-31 16:48:48 +0000

    Fixed the export and noticed small error with service function

--- DebuggingServiceInfo.ps1
+++ DebuggingServiceInfo.ps1
@@ -4,7 +4,7 @@
     [string]$Computername
   )
   $services=Get-WmiObject -Class win32_Service -filter "state='Running'" -computername $computername
-  Write-Host "Found ($services.count) on $computername" -ForegroundColor Green
+  Write-Host "Found $($services.count) on $computername" -ForegroundColor Green
   $services | sort -Property startname,name  | Select -property startname,name,startmode,computername
 }
 
