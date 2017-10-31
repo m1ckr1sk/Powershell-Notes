69d182bef572fd0ee4b85aeb3b55c79ba3c8cffc
Mike Butt <mike.butt@os.uk>
2017-10-31 16:05:40 +0000

    Checking the debugging before answer

---
+++ DebuggingServiceInfo.ps1
@@ -0,0 +1,7 @@
+﻿Function Get-ServiceInfo {
+[cmdletbinding()]
+Param([string]$Computername)
+$services=Get-WmiObject -Class Win32_Services -filter "state='Running" -computername $computernam
+Write-Host "Found ($services.count) on $computername" -ForegroundColor Green
+$sevices | sort -Property startname,name  Select -property startname,name,startmode,computername
+}
