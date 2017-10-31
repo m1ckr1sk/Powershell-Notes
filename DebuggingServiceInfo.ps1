85d7c62a8c74395fa68088a4cf656d694e33d387
Mike Butt <mike.butt@os.uk>
2017-10-31 16:09:21 +0000

    After debugging the first exercise

--- DebuggingServiceInfo.ps1
+++ DebuggingServiceInfo.ps1
@@ -1,7 +1,11 @@
 ﻿Function Get-ServiceInfo {
-[cmdletbinding()]
-Param([string]$Computername)
-$services=Get-WmiObject -Class Win32_Services -filter "state='Running" -computername $computernam
-Write-Host "Found ($services.count) on $computername" -ForegroundColor Green
-$sevices | sort -Property startname,name  Select -property startname,name,startmode,computername
+  [cmdletbinding()]
+  Param(
+    [string]$Computername
+  )
+  $services=Get-WmiObject -Class win32_Service -filter "state='Running'" -computername $computername
+  Write-Host "Found ($services.count) on $computername" -ForegroundColor Green
+  $services | sort -Property startname,name  | Select -property startname,name,startmode,computername
 }
+
+Get-ServiceInfo -Computername 'localhost'
