7d4badd0f9b1ef9b23322295050e3db198faf1a8
Mike Butt <mike.butt@os.uk>
2017-11-01 09:37:59 +0000

    Added small function to get dot net versions installed

--- Tools.ps1
+++ Tools.ps1
@@ -40,6 +40,13 @@ function Get-ServiceInfo{
                     -Filter "State='Running'"
 }
 
+function Get-DotNetVersion{
+    Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' -recurse |
+    Get-ItemProperty -name Version,Release -EA 0 |
+    Where { $_.PSChildName -match '^(?!S)\p{L}'} |
+    Select PSChildName, Version, Release
+}
+
 function Get-AdminPasswordStatusText{
     Param(
         [string]$adminPasswordStatusNumber
