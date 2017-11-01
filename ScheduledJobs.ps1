898851d5201800cd0e7290257b02116e6cd3636c
Mike Butt <mike.butt@os.uk>
2017-11-01 15:50:19 +0000

    Fixed scheduled script to only remove the scheduled job created in the script

---
+++ ScheduledJobs.ps1
@@ -0,0 +1,19 @@
+﻿Import PSScheduledJob
+
+Get-job | Remove-Job
+
+$trigger = New-JobTrigger -Once -At (Get-Date).AddSeconds(30)
+
+Register-ScheduledJob -Trigger $trigger -Name DemoJob -ScriptBlock { Get-EventLog -LogName Application}
+
+Get-ScheduledJob | Select-Object -Expand JobTriggers
+
+Get-ScheduledJob
+
+Get-Job
+
+Receive-Job -Name DemoJob
+
+Get-Job -Name DemoJob | Remove-Job
+
+Get-ScheduledJob -Name DemoJob | Unregister-ScheduledJob
